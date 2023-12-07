6958adc72d8b143b8993d35caa4d9f88ab895841
tkdgh7063 <hoo412298@gmail.com>
2023-12-08 03:20:19 +0900

    🎨 배열 사용

--- app/src/main/java/kr/ac/kumoh/ce/s20180630/s23w1203lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180630/s23w1203lazylist/MainActivity.kt
@@ -11,20 +11,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20180630.s23w1203lazylist.ui.theme.S23W1203LazyListTheme
 
+data class Song(var title: String, var singer: String)
+private val songs = mutableListOf<Song>()
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        repeat(10) {
+            songs.add(Song("테스형", "나훈아"))
+            songs.add(Song("소주 한 잔", "임창정"))
+            songs.add(Song("사랑에 연습이 있었다면", "임재현"))
+        }
+
         setContent {
             MainScreen()
         }
@@ -45,15 +55,15 @@ fun MainScreen() {
 }
 
 @Composable
-fun SongItem(index: Int) {
+fun SongItem(song: Song) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color(0xffffffcc))
             .padding(16.dp)
     ) {
-        TextTitle("노래 $index")
-        TextSinger("이 노래를 부른 가수는 ${index}입니다")
+        TextTitle("노래 ${song.title}")
+        TextSinger("이 노래를 부른 가수는 ${song.singer}입니다")
     }
 }
 
@@ -73,8 +83,8 @@ fun MyList() {
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(horizontal = 8.dp)
     ) {
-        items(30) {
-            SongItem(it)
+        items(songs) { song ->
+            SongItem(song)
         }
     }
 }
